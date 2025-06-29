ed37e3132090775464647c00745eb15820454857
Laura Isel <laura.isel@gmail.com>
2025-06-29 02:29:50 +0200

    init commit

---
+++ linkedin-company/src/main/java/hr/algebra/linkedin_company/LinkedinCompanyApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.linkedin_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinCompanyApplication.class, args);
+	}
+
+}
---
+++ linkedin-company/src/test/java/hr/algebra/linkedin_company/LinkedinCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.linkedin_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
